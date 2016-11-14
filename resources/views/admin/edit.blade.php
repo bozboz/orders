43f47714be345c147885b30fd8b849f8ee214796
Jim Hollington <jim@bozboz.co.uk>
2016-11-14 11:35:16 +0000

    Don't escape form errors

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -31,7 +31,7 @@
 	@endif
 
 		@foreach($fields as $field)
-			 <div class="form-group{{{ ($field->getErrors($errors)) ? ' bs-callout bs-callout-danger' : '' }}}">
+			 <div class="form-group{!! ($field->getErrors($errors)) ? ' bs-callout bs-callout-danger' : '' !!}">
 				{{ $field->getLabel() }}
 				{{ $field->getInput() }}
 				{{ $field->getErrors($errors) }}
